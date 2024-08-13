1399ce28ae983793152be4d9c923ea1b8fed087b
tatiatt <ttata19@freeuni.edu.ge>
2024-08-13 20:06:09 +0400

    bug fix. sending correct property name.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -62,7 +62,7 @@ export default function SignUpForm() {
                                       lastName,
                                       email,
                                       password,
-                                      formattedDate,
+                                      birthday:formattedDate,
                                       parentFirstname,
                                       parentLastname}); 
   
